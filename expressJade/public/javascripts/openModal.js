b4547280eea09a379e7f0ffea6952b0fb1cea128
ofjdev <ofj.dev@gmail.com>
2018-02-21 13:49:47 +0100

    Added th Modal Add Concert Form
    Only added the CSS, Javascript to show and Hide, and JADEHtml Element to get a working Modal Window

--- expressJade/public/javascripts/openModal.js
+++ expressJade/public/javascripts/openModal.js
@@ -1,3 +1,35 @@
+
+
 function openModal(){
-	alert('Modal To Open!');
+	
+	var modalAddForm = document.getElementById('addForm');
+	showBlock(modalAddForm);
+}
+
+function closeModal(){
+	
+	var modalAddForm = document.getElementById('addForm');
+
+	hide(modalAddForm);
+}
+
+// Get the <span> element that closes the modal
+//var span = document.getElementsByClassName("close")[0];
+// When the user clicks on <span> (x), close the modal
+//span.onclick = closeModal;
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	var modalAddForm = document.getElementById('addForm');
+    if (event.target == modalAddForm) {
+        hide(modalAddForm); // closeModal();
+    }
+}
+
+function hide(element){
+	element.style.display = "none";
+}
+
+function showBlock(element){
+	element.style.display = "block";	
 }
